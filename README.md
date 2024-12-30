933285fcf201de05c113c609e2be15d56315fd3b
jrospert <joelrospert@gmail.com>
2024-12-30 12:58:03 -0800

    improved copy files function and README

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 ## Usage
 
+### To copy files from source to destination
+
 ```
-py picture-organizer.py rename_files --file_dir [\location\of\files]
+py picture-organizer.py copy_files --src_dir "\source\directory" --dst_dir "\destination\directory"
+```
+
+### To rename files in a directory
+
+```
+py picture-organizer.py rename_files --file_dir "\location\of\files"
+```
--- copy_files.py
+++ copy_files.py
@@ -11,13 +11,21 @@ class copy_files:
         for file in os.listdir(src):
             source = os.path.join(src, file)
             destination = os.path.join(dst,file)
-            
-            # shutil.copytree retains the exif data
-            # default shutil.copytree syntax
-            # shutil.copytree (src,dst,symlinks=False,ignore=None,copy_function=copy2,ignore_dangling_symlinks=False)
-            # By default, copytree uses shutil.copy2() as the copy function, which retains exif data
-            shutil.copytree(source, destination)            
-           
-              
 
-        
+            # shutil.copytree retains the exif data
+            # default shutil.copytree syntax:
+            #   shutil.copytree (src,dst,symlinks=False,ignore=None,copy_function=copy2,ignore_dangling_symlinks=False)
+            #   By default, copytree uses shutil.copy2() as the copy function, which retains exif data
+            # shutil.ignore_patterns will not copy file types listed in the function
+            try:
+                print(f'Copying folders from [{source}] to [{destination}], excluding AAE and GIF file types.')
+                shutil.copytree(
+                    source,
+                    destination,
+                    ignore = shutil.ignore_patterns('*.AAE', '*.GIF')
+                )
+                print(f'Successfully copied from [{source}] to [{destination}]')
+            except FileExistsError:
+                print(f'WARNING: The folder [{destination}] already exists. Not copying')
+            except Exception as e:
+                print(f'An error occurred: {e}')       
